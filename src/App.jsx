d902007bfd8bc9dfba8c3c9b16df33bc4aaf6cb3
Colby Butler <colby@owljet.com>
2023-07-16 22:51:44 -0400

    v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,84 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [hourlyWage, setHourlyWage] = useState('');
+  const [annualEarnings, setAnnualEarnings] = useState(0);
+  const [taxAmount, setTaxAmount] = useState(0);
+  const [netEarnings, setNetEarnings] = useState(0);
+
+  const calculateAnnualEarnings = () => {
+    const hourlyWageFloat = parseFloat(hourlyWage);
+    const workHoursPerWeek = 40; // Assuming 40 work hours per week
+    const weeksPerYear = 52; // Assuming 52 weeks in a year
+    const paidHolidays = 10; // Assuming 10 paid holidays in a year
+
+    const annualSalary = hourlyWageFloat * workHoursPerWeek * (weeksPerYear - (paidHolidays / 5));
+    setAnnualEarnings(annualSalary.toFixed(2));
+
+    // Tax Calculation for 2023
+    const taxBrackets = [
+      { limit: 9875, rate: 0.1 },
+      { limit: 40125, rate: 0.12 },
+      { limit: 85525, rate: 0.22 },
+      { limit: 163300, rate: 0.24 },
+      { limit: 207350, rate: 0.32 },
+      { limit: 518400, rate: 0.35 },
+      { limit: Infinity, rate: 0.37 }
+    ];
+
+    let remainingIncome = annualSalary;
+    let totalTax = 0;
+
+    for (let i = 0; i < taxBrackets.length; i++) {
+      const { limit, rate } = taxBrackets[i];
+
+      if (remainingIncome <= 0) {
+        break;
+      }
+
+      if (remainingIncome > limit) {
+        const taxableAmount = limit;
+        const tax = taxableAmount * rate;
+        totalTax += tax;
+        remainingIncome -= taxableAmount;
+      } else {
+        const taxableAmount = remainingIncome;
+        const tax = taxableAmount * rate;
+        totalTax += tax;
+        break;
+      }
+    }
+
+    setTaxAmount(totalTax.toFixed(2));
+    setNetEarnings((annualSalary - totalTax).toFixed(2));
+  };
 
   return (
-    <>
+    <div>
+      <h1>Hourly Wage to Annual Earnings Calculator</h1>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <label htmlFor="hourly-wage">Hourly Wage:</label>
+        <input
+          id="hourly-wage"
+          type="number"
+          step="0.01"
+          value={hourlyWage}
+          onChange={(e) => setHourlyWage(e.target.value)}
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <button onClick={calculateAnnualEarnings}>Calculate</button>
+      {annualEarnings > 0 && (
+        <div>
+          <h2>Annual Earnings:</h2>
+          <p>${annualEarnings}</p>
+          <h2>Tax Amount:</h2>
+          <p>${taxAmount}</p>
+          <h2>Net Earnings:</h2>
+          <p>${netEarnings}</p>
+        </div>
+      )}
+    </div>
+  );
+};
 
-export default App
+export default App;
